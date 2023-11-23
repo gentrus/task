f9444e59ed3d7f792543a3ebc9f4720695b88c2c
gentrus <1111ege@mail.ru>
2023-11-23 13:34:54 +0300

    init

--- src/lesson6_3.java
+++ src/lesson6_3.java
@@ -18,12 +18,22 @@ import java.util.Scanner;
                 System.out.println("|");
             }
             dos (a);
-
+            System.out.println();
+            System.out.print("Enter a ");
+            int d = in.nextInt();
+            int e=1;
+            for (int y=1;y<d;y++) {
+                e=e*(y+1);
+            }
+            System.out.print(e);
         }
         static void dos (int a){
             System.out.print(" ");
             for (int i=0;i<a;i++) {
                 System.out.print("-");
-            }
-        }}
+            }}
+
+
+    }
+
 
