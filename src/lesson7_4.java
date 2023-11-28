6213dbd817e7af19008fa881a670bbc75ce63192
gentrus <1111ege@mail.ru>
2023-11-28 13:53:10 +0300

    init

--- src/lesson7_4.java
+++ src/lesson7_4.java
@@ -5,16 +5,17 @@ public class lesson7_4 {
 
         System.out.print("Enter a: ");
         int a = in.nextInt();
+        in.close();
+        if (a<1) {
+            System.out.println("Ошибка выражения");
+        return;}
+
         System.out.println(kor(a));
     }
     static double kor (int a) {
 
         if (a==1) {
             return 1;
-        }
-
-        if (a<1) {
-            System.out.println("Ошибка выражения");
         }
 
         return Math.sqrt(a+Math.sqrt(kor(a-1)));
